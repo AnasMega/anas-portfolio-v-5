d5cc1e1ac9280378e6e98df0a41fa9adee261d02
AnasMega <anas.megatech@gmail.com>
2024-07-06 03:41:48 -0700

    icon updated

--- app/layout.js
+++ app/layout.js
@@ -13,11 +13,16 @@ export const metadata = {
   title: "Portfolio of Anas Hussain-Developer",
   description:
     "This is the portfolio of Anas Hussain. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/static/favicon.ico" style={{ borderRadius: 15 }} />
+
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
