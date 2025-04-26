c3f22aacbb9c32056132c7ba4435ba1ac323f886
Anas Hussain <anas.hussain@youngs.org>
2025-04-26 12:08:27 +0500

    update profile

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Anas Hussain",
   profile: '/profile.png',
-  designation: "MERN Stack-React Native-Frontend Developer",
+  designation: "MERN Stack-React Native-FUll Stack Principle Software Engineer",
   description: "My name is Anas Hussain. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love to write code for api development and love to make the web more open to the world. My core skill is based on React.js Next.js Node.js React Native and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'anashussain0311@gmail.com',
   phone: '+923112996610',
